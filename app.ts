1dca7cd7155df198865a0a57c011d1c2791b263c
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-07-10 20:48:37 +0900

    Tuple型

--- app.ts
+++ app.ts
@@ -2,18 +2,29 @@
 //   name: string
 //   age: number
 // }
-const parson = {
+const person: {
+  name: string
+  age: number
+  hobbies: string[]
+  role: [number, string]
+} = {
   name: 'yota',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 }
+
+// person.role.push('admin')
+// person.role[1] = 10
+
+// person.role = [0, 'adomin', 'user'];
 
 let favoriteActivities: string[]
 favoriteActivities = ['Sports']
 
-console.log(parson.name)
+console.log(person.name)
 
-for (const hobby of parson.hobbies) {
+for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase())
   // console.log(hobby.map())
 }
